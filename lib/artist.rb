30c6f0375ecb7263de1980845299723e00a1f645
jenshaw86 <bloo.j.shaw@gmail.com>
2019-05-17 15:31:25 -0700

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+require "pry"
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(title, genre)
+        Song.new(title, self, genre)
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        artists_songs = self.songs
+        artists_songs.collect {|song| song.genre}
+    end
+end
